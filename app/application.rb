78b1f0dfadfcf0027217c3c21b2d9301b050c8ba
Kat B <katreesebarnes@gmail.com>
2019-06-11 16:49:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
+       elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty
       resp.write "Path Not Found"
     end
 
